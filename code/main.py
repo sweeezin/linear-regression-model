714d28f73cc0591575637943d64fd2e2cae6fb4e
fiona.jin <fiona.y.jin@gmail.com>
2024-12-16 18:35:05 -0800

    add cvs file

--- code/main.py
+++ code/main.py
@@ -6,4 +6,14 @@ import matplotlib as plt
 #data manipulation
 import pandas as pd
 
-#
+import pylab as pl 
+
+import numpy as np
+
+from sklearn import linear_model
+regr = linear_model.LinearRegression()
+
+#import dataset
+
+df = pd.read_csv("Ecommerce_Customers.csv")
+df.head(3)
