31a3862338434f5bc22594a0969d5a99530a4a9f
fiona.jin <fiona.y.jin@gmail.com>
2024-12-16 20:23:23 -0800

    data visualization with matplotlib (scatterplot)

--- code/main.py
+++ code/main.py
@@ -1,13 +1,11 @@
 #import libraries
+import matplotlib
+matplotlib.use('TkAgg')  # Use an interactive backend
 
-#graph/data visualization
-import matplotlib as plt 
+import matplotlib.pyplot as plt
 
-#data manipulation
 import pandas as pd
-
 import pylab as pl 
-
 import numpy as np
 
 from sklearn import linear_model
@@ -15,5 +13,45 @@ regr = linear_model.LinearRegression()
 
 #import dataset
 
-df = pd.read_csv("Ecommerce_Customers.csv")
-df.head(3)
+df = pd.read_csv("/home/fionajin/github/linear-regression-model/code/Ecommerce_Customers.csv")
+print(df.head(3))
+
+cdf = df[["Avg Session Length", "Time on App", 
+               "Time on Website",'Length of Membership',"Yearly Amount Spent"]]
+
+#plotting 
+plt.scatter(cdf[["Length of Membership"]], cdf[["Yearly Amount Spent"]],  color='blue')
+plt.xlabel("Length of Membership")
+plt.ylabel("Yearly Amount Spent")
+plt.show()
+
+msk = np.random.rand(len(df)) < 0.8
+
+train = cdf[msk]
+test = cdf[~msk]
+
+#training
+plt.scatter(train[["Length of Membership"]], train[["Yearly Amount Spent"]],  color='blue')
+plt.xlabel("Length of Membership")
+plt.ylabel("Yearly Amount Spent")
+plt.show()
+
+inputCols=["Avg Session Length", "Time on App", 
+               "Time on Website",'Length of Membership']
+
+x = np.asanyarray(train[inputCols])
+y = np.asanyarray(train[['Yearly Amount Spent']])
+
+regr.fit (x, y)
+# The coefficients
+print ('Coefficients: ', regr.coef_)
+
+y_hat= regr.predict(test[inputCols])
+x = np.asanyarray(test[inputCols])
+y = np.asanyarray(test[['Yearly Amount Spent']])
+print("Residual sum of squares: %.2f"
+      % np.mean((y_hat - y) ** 2))
+
+# Explained variance score: 1 is perfect prediction
+print('Variance score: %.2f' % regr.score(x, y))
+
